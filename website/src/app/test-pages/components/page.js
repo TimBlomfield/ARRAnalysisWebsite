dc9b6788213418bb18d1327d0d4395be90273d38
krspv <31394089+krspv@users.noreply.github.com>
2024-07-17 12:52:25 +0200

    middleware test

--- website/src/app/test-pages/components/page.js
+++ website/src/app/test-pages/components/page.js
@@ -1,6 +1,10 @@
+import {notFound} from 'next/navigation';
 
 
 const ComponentsPage = () => {
+  if (process.env.K_ENVIRONMENT === "Local")
+    notFound();
+
   return (
     <div>
       Components, environment = {process.env.K_ENVIRONMENT}
---
+++ website/src/middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+
+
+const isStagingOrProd = process.env.K_ENVIRONMENT === "Staging" || process.env.K_ENVIRONMENT === "Production";
+
+
+export const middleware = (request) => {
+  const pathname = request.nextUrl.pathname.toLowerCase();
+
+  if (isStagingOrProd && pathname.startsWith('/test-pages'))
+    return NextResponse.redirect(new URL('/ ', request.url));
+  return NextResponse.next();
+};
+
+
+
+// NOTE: Don't use config, it doesn't check for letter-case. Navigating to ./Test-pages will not run the middleware.
+//       Also `export { middleware, config }` does not work!
+
+/*
+export const config = {
+  matcher: ['/test-pages/:path*', '/admin/:path*'],
+};
+*/
