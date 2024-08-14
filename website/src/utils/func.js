130d2c71b0352a87cc82bd36e1b977f0b43ef31a
krspv <31394089+krspv@users.noreply.github.com>
2024-08-14 15:59:02 +0200

    Installed Stripe
    - Also added a new script "devS" for running https locally

--- website/src/utils/func.js
+++ website/src/utils/func.js
@@ -1,5 +1,9 @@
 const generateUniqueId = (prefix = '') => `${prefix}-${Date.now()}-${Math.floor(Math.random()*1_000_000)}`;
 
+const convertToSuburrency = (amount, factor = 100) => Math.round(amount * factor);
+
+
 export {
   generateUniqueId,
+  convertToSuburrency,
 };
