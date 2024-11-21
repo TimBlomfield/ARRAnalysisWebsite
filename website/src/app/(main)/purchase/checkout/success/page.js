304b88e245011465eca9154d5bae5ef4c2375152
krspv <31394089+krspv@users.noreply.github.com>
2024-11-21 16:53:19 +0100

    Typo fix

--- website/src/app/(main)/purchase/checkout/success/page.js
+++ website/src/app/(main)/purchase/checkout/success/page.js
@@ -43,7 +43,7 @@ const PaymentSuccessPage = async ({ searchParams }) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', padding: '20px' }}>
       <h1 style={{ fontSize: '32px', marginTop: '60px' }}>Success! You are subscribed now!</h1>
-      <h3 style={{ fontSize: '18px' }}>You`&apos;ve purchased Tier-{purchaseInfo.tier + 1} {purchaseInfo.period === 0 ? 'Monthly' : 'Yearly'} subscription(s)</h3>
+      <h3 style={{ fontSize: '18px' }}>You&apos;ve purchased Tier-{purchaseInfo.tier + 1} {purchaseInfo.period === 0 ? 'Monthly' : 'Yearly'} subscription(s)</h3>
       {purchaseInfo.tier === 2 && <h3 style={{ fontSize: '18px', marginTop: '10px', color: 'darkred', fontWeight: 'bold' }}>Count: {purchaseInfo.licenses}</h3>}
       <h2 style={{ fontSize: '24px', marginTop: '40px', fontStyle: 'italic', color: 'teal' }}>You can now login as a customer!</h2>
     </div>
