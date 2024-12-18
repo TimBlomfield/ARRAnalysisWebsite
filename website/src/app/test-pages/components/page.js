991e26a83317d0ca1af46e0639375213a44085b5
krspv <31394089+krspv@users.noreply.github.com>
2024-12-18 20:46:21 +0100

    Updated the test components page

--- website/src/app/test-pages/components/page.js
+++ website/src/app/test-pages/components/page.js
@@ -10,6 +10,7 @@ import IconButton from '@/components/IconButton';
 import LinkButton from '@/components/LinkButton';
 import Loading from '@/components/Loading';
 import LoadingSSR from '@/components/LoadingSSR';
+import MultiToggle from '@/components/MultiToggle';
 import PlusMinusButton from '@/components/PlusMinusButton';
 import PushButton from '@/components/PushButton';
 // Images
@@ -21,6 +22,30 @@ import styles from './styles.module.scss';
 const ComponentsPage = () => {
 
   const [plusMinusValue, setPlusMinusValue] = useState(0);
+  const [multiToggle_01, setMultiToggle_01] = useState(0);
+  const [multiToggle_02, setMultiToggle_02] = useState(0);
+  const [multiToggle_03, setMultiToggle_03] = useState(0);
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Loading Section
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  const LoadingSection_MemoRender = useMemo(() => (
+    <>
+      <div className={styles.title}>&lt;Loading size={'{1}'} text=&quot;Hello&quot; /&gt;</div>
+      <div className={styles.loadingGrid}>
+        <div className={styles.cell}><Loading /></div>
+        <div className={styles.cell}><Loading scale={1.5} /></div>
+        <div className={styles.cell}><Loading scale={2} /></div>
+        <div className={styles.cell}><Loading text="Loading..." /></div>
+        <div className={styles.cell}><Loading scale={2} text="Loading..." /></div>
+        <div className={cn(styles.cell, styles.wider)}><Loading text="Lorem ipsum dolor sit amet consectetur adipiscing elit..." /></div>
+        <div className={styles.cell}><Loading scale={2} text="Lorem ipsum dolor sit amet consectetur adipiscing elit..." /></div>
+        <div className={styles.cell}><LoadingSSR theme={K_Theme.Dark} scale={1} text="Howdy" /></div>
+        <div className={cn(styles.cell, styles.wider)}><LoadingSSR scale={2} text="Lorem ipsum dolor sit amet consectetur adipiscing elit..." /></div>
+      </div>
+    </>
+  ), []);
 
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -167,53 +192,100 @@ const ComponentsPage = () => {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // IconButton Section
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  const IconButtonSection_MemoRender = useMemo(() => {
-    return (
-      <>
-        <div className={cn(styles.title, styles.pt)}>&lt;IconButton /&gt;</div>
-        <div className={styles.iconButtonGrid}>
-          {Object.values(K_Theme).map(theme => (
-            <>
-              <div className={styles.child}>
-                <IconButton theme={theme} svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} />
-              </div>
-              <div className={styles.child}>
-                <IconButton theme={theme} svg={PlusSvg} onClick={() => toast('Icon Button!')} />
-              </div>
-              <div className={styles.child}>
-                <IconButton theme={theme} svg={PlusSvg} scale={1.8} invertBkTheme
-                            onClick={() => toast('Icon Button!')} />
-              </div>
-              <div className={styles.child}>
-                <IconButton theme={theme} svg={PlusSvg} scale={1.3} svgScale={1.5}
-                            onClick={() => toast('Icon Button!')} />
-              </div>
-              <div className={styles.child}>
-                <IconButton theme={theme} svg={PlusSvg} scale={1.3} svgScale={1.5} disabled
-                            onClick={() => toast('Icon Button!')} />
-              </div>
-              <div className={styles.child}>
-                <IconButton theme={theme} svg={PlusSvg} scale={1.3} svgScale={1.5} disabled onClick={() => toast('Icon Button!')} />
-              </div>
-            </>
-          ))}
-          {[false,true].map(d => (
-            <>
-              <div className={styles.child}>
-                <IconButton theme={K_Theme.Light} transparent svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} disabled={d} />
-              </div>
-              <div className={styles.child}>
-                <IconButton theme={K_Theme.Dark} transparent svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} disabled={d} />
-              </div>
-              <div className={styles.child}>
-                <IconButton theme={K_Theme.Danger} transparent svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} disabled={d} />
-              </div>
-            </>
-          ))}
+  const IconButtonSection_MemoRender = useMemo(() => (
+    <>
+      <div className={cn(styles.title, styles.pt)}>&lt;IconButton /&gt;</div>
+      <div className={styles.iconButtonGrid}>
+        {Object.values(K_Theme).map(theme => (
+          <>
+            <div className={styles.child}>
+              <IconButton theme={theme} svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} />
+            </div>
+            <div className={styles.child}>
+              <IconButton theme={theme} svg={PlusSvg} onClick={() => toast('Icon Button!')} />
+            </div>
+            <div className={styles.child}>
+              <IconButton theme={theme} svg={PlusSvg} scale={1.8} invertBkTheme
+                          onClick={() => toast('Icon Button!')} />
+            </div>
+            <div className={styles.child}>
+              <IconButton theme={theme} svg={PlusSvg} scale={1.3} svgScale={1.5}
+                          onClick={() => toast('Icon Button!')} />
+            </div>
+            <div className={styles.child}>
+              <IconButton theme={theme} svg={PlusSvg} scale={1.3} svgScale={1.5} disabled
+                          onClick={() => toast('Icon Button!')} />
+            </div>
+            <div className={styles.child}>
+              <IconButton theme={theme} svg={PlusSvg} scale={1.3} svgScale={1.5} disabled onClick={() => toast('Icon Button!')} />
+            </div>
+          </>
+        ))}
+        {[false,true].map(d => (
+          <>
+            <div className={styles.child}>
+              <IconButton theme={K_Theme.Light} transparent svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} disabled={d} />
+            </div>
+            <div className={styles.child}>
+              <IconButton theme={K_Theme.Dark} transparent svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} disabled={d} />
+            </div>
+            <div className={styles.child}>
+              <IconButton theme={K_Theme.Danger} transparent svg={PlusSvg} invertBkTheme onClick={() => toast('Icon Button!')} disabled={d} />
+            </div>
+          </>
+        ))}
+      </div>
+    </>
+  ), []);
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // MultiToggle Section
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  const MultiToggleSection_MemoRender = useMemo(() => (
+    <>
+      <div className={cn(styles.title, styles.pt)}>&lt;MultiToggle /&gt;</div>
+      <div className={styles.multiToggleGrid}>
+        <div className={styles.cell}>
+          <MultiToggle selected={multiToggle_01}
+                       options={['Suscipit', 'Erat', 'Vestibulum', 'Ullamcorper ', 'Cras']}
+                       onSelect={x => setMultiToggle_01(x)} />
         </div>
-      </>
-    );
-  }, []);
+        <div className={styles.cell}>
+          <MultiToggle extraClass={cn(styles.xtraMT, styles.otherBk)}
+                       selected={multiToggle_02}
+                       options={['Suscipit', 'Erat', 'Vestibulum', 'Ullamcorper ', 'Cras']}
+                       onSelect={x => setMultiToggle_02(x)} />
+        </div>
+        <div className={styles.cell}>
+          <MultiToggle theme={K_Theme.Light}
+                       selected={multiToggle_03}
+                       options={['A', 'B', 'C']}
+                       onSelect={x => setMultiToggle_03(x)} />
+        </div>
+        <div className={styles.cell}>
+          <MultiToggle selected={multiToggle_01}
+                       options={['Suscipit', 'Erat', 'Vestibulum', 'Ullamcorper ', 'Cras']}
+                       onSelect={x => setMultiToggle_01(x)}
+                       disabled />
+        </div>
+        <div className={styles.cell}>
+          <MultiToggle extraClass={styles.xtraMT}
+                       selected={multiToggle_02}
+                       options={['Suscipit', 'Erat', 'Vestibulum', 'Ullamcorper ', 'Cras']}
+                       onSelect={x => setMultiToggle_02(x)}
+                       disabled />
+        </div>
+        <div className={styles.cell}>
+          <MultiToggle theme={K_Theme.Light}
+                       selected={multiToggle_03}
+                       options={['A', 'B', 'C']}
+                       onSelect={x => setMultiToggle_03(x)}
+                       disabled />
+        </div>
+      </div>
+    </>
+  ), [multiToggle_01, multiToggle_02, multiToggle_03]);
 
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -228,25 +300,15 @@ const ComponentsPage = () => {
     );
   }, []);
 
+
   return (
     <main className={styles.main}>
-      <div className={styles.title}>&lt;Loading size={'{1}'} text=&quot;Hello&quot; /&gt;</div>
-      <section className={styles.loadingGrid}>
-        <div className={styles.cell}><Loading /></div>
-        <div className={styles.cell}><Loading scale={1.5} /></div>
-        <div className={styles.cell}><Loading scale={2} /></div>
-        <div className={styles.cell}><Loading text="Loading..." /></div>
-        <div className={styles.cell}><Loading scale={2} text="Loading..." /></div>
-        <div className={cn(styles.cell, styles.wider)}><Loading text="Lorem ipsum dolor sit amet consectetur adipiscing elit..." /></div>
-        <div className={styles.cell}><Loading scale={2} text="Lorem ipsum dolor sit amet consectetur adipiscing elit..." /></div>
-        <div className={styles.cell}><LoadingSSR theme={K_Theme.Dark} scale={1} text="Howdy" /></div>
-        <div className={cn(styles.cell, styles.wider)}><LoadingSSR scale={2} text="Lorem ipsum dolor sit amet consectetur adipiscing elit..." /></div>
-      </section>
-
+      {LoadingSection_MemoRender}
       {PushButtonSection_MemoRender}
       {LinkButtonSection_MemoRender}
       {PlusMinusButtonSection_MemoRender}
       {IconButtonSection_MemoRender}
+      {MultiToggleSection_MemoRender}
       {ComboBoxSection_MemoRender}
     </main>
   );
